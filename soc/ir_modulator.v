2525ade422c050ed68471d261e01298426a60b5d
Chris Gervang <chris@gervang.com>
2019-11-15 23:48:10 -0800

    clock parameter speed

--- soc/ir_modulator.v
+++ soc/ir_modulator.v
@@ -1,4 +1,6 @@
-module ir_modulator (
+module ir_modulator #(
+	parameter CLOCK_SPEED=8000000 // 8MHz
+)(
 	input clk,
 	output out
 );
@@ -7,7 +9,7 @@ reg [23:0] carrier; // 24-bit counter
 reg carrier_out = 0;
 
 always @(posedge clk) begin
-	if (carrier < 105) // 38KHz. 8000000 / 2 / 38000
+	if (carrier < CLOCK_SPEED / 2 / 38000) // 38KHz. i.e. 8MHz is 8000000 / 2 / 38000 = 105
 	begin
 		carrier <= carrier + 1;
 	end else 
